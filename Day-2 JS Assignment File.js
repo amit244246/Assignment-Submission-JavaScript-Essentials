05c90b6a72d36904c2c71bfb453c60a00edf103a
amit244246 <79333747+amit244246@users.noreply.github.com>
2021-07-07 06:37:26 +0530

    Add files via upload

---
+++ Day-2 JS Assignment File.js
@@ -0,0 +1,15 @@
+//Day-2 JS Essentials Assignment code by Amit Sharma
+
+//A program to print 5 times "*" horizontally in a single row
+
+for (i = 0; i <= 0; i++){
+    var value = "";
+
+    for (j = 0; j <= 4; j++){
+        value=value+" *";
+    }
+
+console.log(value);
+}
+
+//Output: * * * * *
